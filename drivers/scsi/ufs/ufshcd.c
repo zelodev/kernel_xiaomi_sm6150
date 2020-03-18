b96d68bc045ebe741677925c29365075ecf1c881
Jaegeuk Kim <jaegeuk@google.com>
2020-03-18 12:54:40 -0700

    scsi: ufs: disable hibern8_on_idle
    Let's follow other pixels.
    
    Bug: 151321015
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Change-Id: I5337f0a8a0e75a42cd5d6075d2882cc68f4d0577

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -3003,8 +3003,7 @@ static void ufshcd_init_hibern8_on_idle(struct ufs_hba *hba)
 	/* initialize the state variable here */
 	hba->hibern8_on_idle.state = HIBERN8_EXITED;
 
-	if (!ufshcd_is_hibern8_on_idle_allowed(hba) &&
-	    !ufshcd_is_auto_hibern8_supported(hba))
+	if (!ufshcd_is_hibern8_on_idle_allowed(hba))
 		return;
 
 	if (ufshcd_is_auto_hibern8_supported(hba)) {
@@ -3048,8 +3047,7 @@ static void ufshcd_init_hibern8_on_idle(struct ufs_hba *hba)
 
 static void ufshcd_exit_hibern8_on_idle(struct ufs_hba *hba)
 {
-	if (!ufshcd_is_hibern8_on_idle_allowed(hba) &&
-	    !ufshcd_is_auto_hibern8_supported(hba))
+	if (!ufshcd_is_hibern8_on_idle_allowed(hba))
 		return;
 	device_remove_file(hba->dev, &hba->hibern8_on_idle.delay_attr);
 	device_remove_file(hba->dev, &hba->hibern8_on_idle.enable_attr);
