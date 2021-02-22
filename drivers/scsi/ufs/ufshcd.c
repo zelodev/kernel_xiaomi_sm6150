dd62a0bab9e5f84dcd7cb9d50654c948ce283aff
Sultan Alsawaf <sultan@kerneltoast.com>
2021-02-22 00:43:24 -0800

    scsi: ufs: Add simple IRQ-affined PM QoS operations
    Qualcomm's PM QoS solution suffers from a number of issues: applying
    PM QoS to all CPUs, convoluted spaghetti code that wastes CPU cycles,
    and keeping PM QoS applied for 10 ms after all requests finish
    processing.
    
    This implements a simple IRQ-affined PM QoS mechanism for each UFS
    adapter which uses atomics to elide locking, and enqueues a worker to
    apply PM QoS to the target CPU as soon as a command request is issued.
    
    Change-Id: I634873deb7baf0916208809c025eb0d3ccdb0fa3
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -3990,7 +3990,8 @@ static int ufshcd_queuecommand(struct Scsi_Host *host, struct scsi_cmnd *cmd)
 			if (has_read_lock)
 				ufshcd_put_read_lock(hba);
 			cmd->scsi_done(cmd);
-			return 0;
+			err = 0;
+			goto out_pm_qos;
 		}
 		goto out;
 	}
@@ -6547,6 +6548,9 @@ static void __ufshcd_transfer_req_compl(struct ufs_hba *hba,
 			/* Mark completed command as NULL in LRB */
 			lrbp->cmd = NULL;
 			hba->ufs_stats.clk_rel.ctx = XFR_REQ_COMPL;
+			if (cmd->request) {
+				ufshcd_pm_qos_put(hba);
+			}
 
 			req = cmd->request;
 			if (req) {
@@ -6624,6 +6628,14 @@ void ufshcd_abort_outstanding_transfer_requests(struct ufs_hba *hba, int result)
 			update_req_stats(hba, lrbp);
 			/* Mark completed command as NULL in LRB */
 			lrbp->cmd = NULL;
+			if (cmd->request) {
+				/*
+				 * As we are accessing the "request" structure,
+				 * this must be called before calling
+				 * ->scsi_done() callback.
+				 */
+				ufshcd_pm_qos_put(hba);
+			}
 			clear_bit_unlock(index, &hba->lrb_in_use);
 			ufshcd_release_all(hba);
 
