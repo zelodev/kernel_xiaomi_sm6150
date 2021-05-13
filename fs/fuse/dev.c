049df95d3861123c46aa61ac6ca1927b990b7b35
Alessio Balsini <balsini@google.com>
2021-05-13 11:58:16 +0100

    ANDROID: fuse/passthrough: API V2 with __u32 open argument
    The initial FUSE passthrough interface has the issue of introducing an
    ioctl which receives as a parameter a data structure containing a
    pointer. What happens is that, depending on the architecture, the size
    of this struct might change, and especially for 32-bit userspace running
    on 64-bit kernel, the size mismatch results into different a single
    ioctl the behavior of which depends on the data that is passed (e.g.,
    with an enum). This is just a poor ioctl design as mentioned by Arnd
    Bergmann [1].
    
    Introduce the new FUSE_PASSTHROUGH_OPEN ioctl which only gets the fd of
    the lower file system, which is a fixed-size __u32, dropping the
    confusing fuse_passthrough_out data structure.
    
    [1] https://lore.kernel.org/lkml/CAK8P3a2K2FzPvqBYL9W=Yut58SFXyetXwU4Fz50G5O3TsS0pPQ@mail.gmail.com/
    
    Bug: 175195837
    Signed-off-by: Alessio Balsini <balsini@google.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Change-Id: I486d71cbe20f3c0c87544fa75da4e2704fe57c7c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -2288,7 +2288,6 @@ static long fuse_dev_ioctl(struct file *file, unsigned int cmd,
 	int res;
 	int oldfd;
 	struct fuse_dev *fud = NULL;
-	struct fuse_passthrough_out pto;
 
 	switch (cmd) {
 	case FUSE_DEV_IOC_CLONE:
@@ -2318,13 +2317,11 @@ static long fuse_dev_ioctl(struct file *file, unsigned int cmd,
 		break;
 	case FUSE_DEV_IOC_PASSTHROUGH_OPEN:
 		res = -EFAULT;
-		if (!copy_from_user(&pto,
-				    (struct fuse_passthrough_out __user *)arg,
-				    sizeof(pto))) {
+		if (!get_user(oldfd, (__u32 __user *)arg)) {
 			res = -EINVAL;
 			fud = fuse_get_dev(file);
 			if (fud)
-				res = fuse_passthrough_open(fud, &pto);
+				res = fuse_passthrough_open(fud, oldfd);
 		}
 		break;
 	default:
