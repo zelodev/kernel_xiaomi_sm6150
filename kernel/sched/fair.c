f6b1c9f1f78169d41ea5bc6507aff7930633a5c1
ZeloIsGay <wabafit@outlook.com>
2023-06-06 15:17:46 +0300

    sched: changes for more throughput

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -131,7 +131,7 @@ static unsigned int sched_nr_latency = 20;
  * After fork, child runs first. If set to 0 (default) then
  * parent will (try to) run first.
  */
-unsigned int sysctl_sched_child_runs_first = 1;
+unsigned int sysctl_sched_child_runs_first = 0;
 
 /*
  * To enable/disable energy aware feature.
@@ -9665,7 +9665,7 @@ static struct task_struct *detach_one_task(struct lb_env *env)
 	return NULL;
 }
 
-static const unsigned int sched_nr_migrate_break = 32;
+static const unsigned int sched_nr_migrate_break = 64;
 
 /*
  * detach_tasks() -- tries to detach up to imbalance weighted load from
