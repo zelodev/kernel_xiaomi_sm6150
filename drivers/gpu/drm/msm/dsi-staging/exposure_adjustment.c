d789f4993bd8c817c1e15a99b4f10d46ce61ae44
ZeloIsGay <wabafit@outlook.com>
2023-07-23 02:21:07 +0300

    exposure_adjustment: return real backlight level

--- drivers/gpu/drm/msm/dsi-staging/exposure_adjustment.c
+++ drivers/gpu/drm/msm/dsi-staging/exposure_adjustment.c
@@ -115,12 +115,9 @@ u32 ea_panel_calc_backlight(u32 bl_lvl)
 {
 	last_level = bl_lvl;
 
-	if (pcc_backlight_enable && bl_lvl != 0 && bl_lvl < ELVSS_OFF_THRESHOLD) {
+	if (pcc_backlight_enable && bl_lvl != 0 && bl_lvl < ELVSS_OFF_THRESHOLD)
 		if (ea_panel_send_pcc(bl_lvl))
 			pr_err("ERROR: Failed to send PCC\n");
 
-		return ELVSS_OFF_THRESHOLD;
-	} else {
-		return bl_lvl;
-	}
+	return bl_lvl;
 }
