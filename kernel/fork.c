d3a5628fc7ab0ff7767021d3f818ec346e781e2e
ZeloIsGay <wabafit@outlook.com>
2023-11-26 22:49:27 +0200

    Partially revert 4eebdae25de57b02c7ff04e3f06bb0eadb8ccbcc
    we're going back to cpu-boost

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,7 +95,6 @@
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
-#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
@@ -2241,7 +2240,6 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
-		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 50);
 	}
 
