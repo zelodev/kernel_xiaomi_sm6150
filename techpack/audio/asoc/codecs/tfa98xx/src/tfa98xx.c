712575bca10bdb576208c8363a34d0799fa8028c
PIPIPIG233666 <pig.priv@gmail.com>
2023-11-26 13:48:18 +0100

    techpack: audio: Promote send_tfa_cal_apr out of debugfs codes
    Change-Id: I95be412e79dfdd9e29785ac1022fcc7664cdcdff

--- techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
@@ -393,6 +393,16 @@ static void tfa98xx_inputdev_unregister(struct tfa98xx *tfa98xx)
 	__tfa98xx_inputdev_check_register(tfa98xx, true);
 }
 
+#ifdef TFA_NON_DSP_SOLUTION
+extern int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead);
+#else
+int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead)
+{
+	pr_info("this function is empty!!!\n");
+	return 0;
+}
+#endif
+
 #ifdef CONFIG_DEBUG_FS
 /* OTC reporting
  * Returns the MTP0 OTC bit value
